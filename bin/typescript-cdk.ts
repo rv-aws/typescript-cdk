563a89a03faaeac9262fe98d9954f12eb86d1ccc
venerari <venerayan@gmail.com>
2022-04-15 12:07:33 -0400

    apr 15 working 1

--- bin/typescript-cdk.ts
+++ bin/typescript-cdk.ts
@@ -2,6 +2,16 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { TypescriptCdkStack } from '../lib/typescript-cdk-stack';
+import { Tags } from '@aws-cdk/core';
 
 const app = new cdk.App();
-new TypescriptCdkStack(app, 'TypescriptCdkStack');
+const stack = new TypescriptCdkStack(app, 'TypescriptCdkStack');
+Tags.of(stack).add('App', 'DocumentManagement');
+Tags.of(stack).add('Environment', 'Development');
+
+// import 'source-map-support/register';
+// import * as cdk from '@aws-cdk/core';
+// import { TypescriptCdkStack } from '../lib/typescript-cdk-stack';
+
+// const app = new cdk.App();
+// new TypescriptCdkStack(app, 'TypescriptCdkStack');
